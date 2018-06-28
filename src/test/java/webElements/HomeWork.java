b08c9a9619204ff7dfe622873982dbcf87a2b5d9
Damira Abraimova <damkavalet@mail.ru>
2018-06-27 20:56:02 -0400

    homework

--- src/test/java/webElements/HomeWork.java
+++ src/test/java/webElements/HomeWork.java
@@ -1,21 +1,23 @@
 package webElements;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.github.javafaker.Faker;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class WebElements {
-
+public class HomeWork {
 	WebDriver driver;
 
 	@BeforeClass //runs once for all tests
@@ -26,23 +28,10 @@ public class WebElements {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
+
+	
 	@Test
-	public void myLinks() {
-		driver.get("https://github.com");
-		List<WebElement> links=driver.findElements(By.xpath("//a"));
-		int numberOfTheLinks=links.size();
-		List<String> list=new ArrayList<>();
-		System.out.println("numberOfTheLinks : "+numberOfTheLinks);
-		for (WebElement link : links) {
-			if(!link.getText().isEmpty())
-			System.out.println(link.getText());
-			list.add(link.getText());
-		}
-			System.out.println(list);
-			
-	}
-	@Test
-	public void seleniumWebElementForm() {
+	public void seleniumWebElementForm() throws InterruptedException {
 		driver.get("https://forms.zohopublic.com/murodil/form/"
 				+ "SeleniumWebElements/formperma/"
 				+ "eCecYgX4WMcmjxvXVq6UdhA2ABXIoqPAxnAF8H8CCJg");
@@ -53,40 +42,44 @@ public class WebElements {
 		 List <WebElement> checkBoxes=driver.findElements(By.xpath("//input[@type='checkbox']"));
 		 List <WebElement> radioButtons=driver.findElements(By.xpath("//input[@type='radio']"));
 		 List <WebElement> buttons=driver.findElements(By.tagName("button"));
-		 Assert.assertEquals(list.size(), 2);
-		 Assert.assertEquals(DropDowns.size(), 3);
-		 Assert.assertEquals(checkBoxes.size(), 9);
-		 Assert.assertEquals(radioButtons.size(), 9, "Message will show if it fails");
-		 Assert.assertEquals(buttons.size(), 1 , "Message will show if it fails");
-		 
-		 
-		
-	}
-	@Test
-	public void slideShow() throws InterruptedException {
-		driver.get("https://www.hbloom.com/Gifts/birthday-flowers");
-		List<WebElement> images = driver.findElements(By.tagName("img"));
-		List<String> srcs = new ArrayList<>();
-		
-		for(WebElement flower: images) {
-			srcs.add(flower.getAttribute("src"));
+		 Faker f=new Faker();
+		 for (WebElement lists : list) {
+			 lists.sendKeys(f.name().firstName());
+			
 		}
 	
-		for (String link : srcs) {
-			driver.get(link);
-			Thread.sleep(1234);
+		 for (int i = 0; i < DropDowns.size(); i++) {
+			 
+			Select s=new Select(DropDowns.get(i));
+			s.selectByIndex(f.number().numberBetween(1, 4));
+			
 		}
-	
+//		 for (WebElement eachCheckBox : checkBoxes) {
+//			 eachCheckBox.click();
+//		}
+		 for (int i = 0; i < checkBoxes.size(); i+=3) {
+			
+			 checkBoxes.get(f.number().numberBetween(0, 3)+i).click();
+		}
+		 for (int i = 0; i < radioButtons.size(); i++) {
+			 radioButtons.get(i).click();
+			 Thread.sleep(1000);
+			
+		}
+		 for (WebElement eachButton : buttons) {
+			eachButton.click();
+		}
+		 driver.close();
+		 
 	}
+	
 	/**
 	 * * Homework:
-         *     Loop through each inputbox and enter random names
-         *  Loop through each dropDown and randomly select by index
-         *  Loop through each checkBoxes and select each one
+         *     Loop through each inputbox and enter random names√
+         *  Loop through each dropDown and randomly select by index√
+         *  Loop through each checkBoxes and select each one√
          *  Loop through each radioButton and click one by one by waiting one second intervals
          *  click all buttons
          */
 	
-	
-	 
 }
